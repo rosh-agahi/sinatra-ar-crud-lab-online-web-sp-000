54ca1438d67cc7f96e3c0a3eaf41443acd8a23b7
Roshanak Agahi <rosh.agahi@gmail.com>
2020-12-27 00:02:07 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,48 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+  
+  get '/' do
+  end
+
+  get '/articles' do
+    @articles = Article.all 
+
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    Article.create(params)
+
+    redirect to "/articles/#{Article.last.id}"
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    @article.update(title: params[:title], content: params[:content])
+  
+    redirect to "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    @article.delete
+    redirect to '/articles'
+  end
+
+  
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,10 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+configure :development do
+  set :database, 'sqlite3:db/articles.db'
+end
+
 def fi_check_migration
   begin
     ActiveRecord::Migration.check_pending!
